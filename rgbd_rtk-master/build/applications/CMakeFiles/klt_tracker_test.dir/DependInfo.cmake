70ac3b3ee75f069a3d6a8f08d06c27f19a3a3563
Luiz Felipe Maciel Correia <y9luiz@hotmail.com>
2017-05-05 14:14:58 -0300

    Commit inicial

---
+++ rgbd_rtk-master/build/applications/CMakeFiles/klt_tracker_test.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/luiz/Faculdade/Pesquisa/master/rgbd_rtk-master/applications/klt_tracker_test.cpp" "/home/luiz/Faculdade/Pesquisa/master/rgbd_rtk-master/build/applications/CMakeFiles/klt_tracker_test.dir/klt_tracker_test.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "DISABLE_DAVIDSDK"
+  "DISABLE_DSSDK"
+  "DISABLE_ENSENSO"
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_OPENNI"
+  "DISABLE_OPENNI2"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "DISABLE_RSSDK"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/luiz/Faculdade/Pesquisa/master/rgbd_rtk-master/build/io/CMakeFiles/rgbd_rtk_io.dir/DependInfo.cmake"
+  "/home/luiz/Faculdade/Pesquisa/master/rgbd_rtk-master/build/tracking/CMakeFiles/rgbd_rtk_tracking.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "/usr/include/vtk-5.8"
+  "/home/bruno/Lib/opencv-2.4.13/local/include"
+  "/usr/local/include/pcl-1.8"
+  "/usr/local/include/eigen3"
+  "../common"
+  "../io"
+  "../tracking"
+  "../motion_estimation"
+  "../visualization"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ rgbd_rtk-master/build/applications/CMakeFiles/monocular_image_grabber.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/luiz/Faculdade/Pesquisa/master/rgbd_rtk-master/applications/monocular_image_grabber.cpp" "/home/luiz/Faculdade/Pesquisa/master/rgbd_rtk-master/build/applications/CMakeFiles/monocular_image_grabber.dir/monocular_image_grabber.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "DISABLE_DAVIDSDK"
+  "DISABLE_DSSDK"
+  "DISABLE_ENSENSO"
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_OPENNI"
+  "DISABLE_OPENNI2"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "DISABLE_RSSDK"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "/usr/include/vtk-5.8"
+  "/home/bruno/Lib/opencv-2.4.13/local/include"
+  "/usr/local/include/pcl-1.8"
+  "/usr/local/include/eigen3"
+  "../common"
+  "../io"
+  "../tracking"
+  "../motion_estimation"
+  "../visualization"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
